8cce9c62e6dcac9c4d0db7d93e466e130ed7ece7
xionglongztz <1325452715@qq.com>
2026-08-06 20:10:51 +0800

    fix: certum fingerprint

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -28,5 +28,5 @@ Imports System.Runtime.InteropServices
 '      修订号
 '
 
-<Assembly: AssemblyVersion("1.0.149.0")>
-<Assembly: AssemblyFileVersion("1.0.149.0")>
+<Assembly: AssemblyVersion("1.0.1410")>
+<Assembly: AssemblyFileVersion("1.0.1410.0")>
