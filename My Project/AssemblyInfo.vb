607587702b61f5c332b9565b8815329900d13d7c
xionglongztz <1325452715@qq.com>
2026-08-06 20:11:41 +0800

    version:1410

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -28,5 +28,5 @@ Imports System.Runtime.InteropServices
 '      修订号
 '
 
-<Assembly: AssemblyVersion("1.0.1410")>
+<Assembly: AssemblyVersion("1.0.1410.0")>
 <Assembly: AssemblyFileVersion("1.0.1410.0")>
